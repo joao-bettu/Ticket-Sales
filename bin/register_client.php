7c1020656a5bb65cbf97587d2a85e59228369237
joao-bettu <joaobettuvictor@gmail.com>
2025-07-21 13:08:29 -0300

    Implementado validação de segurança no registro de usuário

--- bin/register_client.php
+++ bin/register_client.php
@@ -9,10 +9,4 @@ session_start();
 
 $client = new Client($db, "clientes");
 
-$client->create([
-    "nome" => $_POST["client-name"],
-    "email" => $_POST["client-email"],
-    "senha" => $_POST["client-password"]
-])
-
 ?>
--- bin/register_user.php
+++ bin/register_user.php
@@ -9,12 +9,36 @@ session_start();
 
 $user = new User($db, "usuarios");
 
-$user->create([
-    "nome" => $_POST["user-name"],
-    "email" => $_POST["user-email"],
-    "senha" => $_POST["user-password"],
-    "editar" => $_POST["edit-ticket"],
-    "deletar" => $_POST["delete-ticket"]
-])
+$errors = [];
+
+$name =  $_POST["user-name"] ?? '';
+$clean_name = filter_var($name, FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);
+if(empty(trim($clean_name))){
+    $errors[] = "O nome é obrigatório!";
+}
+
+$email =  $_POST["user-email"] ?? '';
+$clean_email = filter_var($email, FILTER_VALIDATE_EMAIL);
+if(!$clean_email){
+    $errors[] = "Digite um e-mail válido!";
+}
+
+$password = $_POST["user-password"] ?? '';
+if(strlen($password) < 8){
+    $errors[] = "A senha deve ter pelo menos 8 caracteres.";
+}
+$hash_password = password_hash($password, PASSWORD_BCRYPT);
+
+$edit = filter_var($_POST["edit-ticket"] ?? false, FILTER_VALIDATE_BOOL);
+$delete = filter_var($_POST["delete-ticket"] ?? false, FILTER_VALIDATE_BOOL);
+
+if(empty($errors)){
+    // Criação do usuário no banco de dados
+} else {
+    $mensagem = implode("<br>", $errors);
+    echo $mensagem;
+    header("Location: register_user.php");
+    exit;
+}
 
 ?>
